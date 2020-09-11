d4ef43f3f1ffd8e6ef84181fbdedbd0257dff188
Minh Doan <daywednes@gmail.com>
2020-09-11 05:24:55 -0700

    add validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { AuthCredentialsDto } from './auth-credentials.dto';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
