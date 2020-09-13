06bb344d6de6ce51950c129e0aff581d0f36bdb2
Minh Doan <daywednes@gmail.com>
2020-09-13 00:29:42 -0700

    add test in development mode

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const logger = new Logger('bootstratp');
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   const serverConfig = config.get('server');
   await app.listen(process.env.PORT || serverConfig.port);
   logger.log(`Application started listening to port ${serverConfig.port}`);
