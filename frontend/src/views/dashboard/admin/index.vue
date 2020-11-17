062ebe8287f590118106172735bbe1bf25ff6e14
hoangnhutvu <vu23071997@gmail.com>
2020-11-18 01:08:06 +0700

    Update Lastest Activity and Last 12 Hourse

--- frontend/src/views/dashboard/admin/index.vue
+++ frontend/src/views/dashboard/admin/index.vue
@@ -19,13 +19,38 @@
         </div>
       </el-col>
       <el-col :xs="24" :sm="24" :lg="10">
-        <div class="chart-wrapper" style="color:white; height:140px; background: #101f3e;">
+        <div
+          class="chart-wrapper"
+          style="color:white; height:140px; background: #101f3e;"
+        >
           WEATHER
           <br />
-          <img v-if="weatherimg" :src="weatherimg"  style="height:90px;" />
+          <img v-if="weatherimg" :src="weatherimg" style="height:90px;" />
         </div>
-        <div class="chart-wrapper" style="height:312px;">
+        <div class="chart-wrapper" style="height:312px; overflow-x: auto;">
           LASTEST ACTIVITY
+          <el-row
+            :gutter="32"
+            v-for="(item, index) of items"
+            :key="index"
+            style="width: 100%;
+            text-align: left; margin: 10px; padding: 5px;"
+          >
+            <el-col :xs="4" :sm="4" :lg="4">
+              <img v-if="logo" :src="logo" style="float: left; height: 50px;" />
+            </el-col>
+            <el-col :xs="8" :sm="16" :lg="16">
+              {{ item.name }}
+              <br />
+              {{ item.status }}
+            </el-col>
+            <el-col :xs="4" :sm="4" :lg="4">
+              {{ item.time }}
+            </el-col>
+            <hr
+              style=" position: absolute;left: 0;bottom: 0;width: 100%;margin: 0;"
+            />
+          </el-row>
         </div>
       </el-col>
       <el-col :xs="24" :sm="24" :lg="4">
@@ -54,8 +79,13 @@
         </div>
       </el-col>
       <el-col :xs="24" :sm="24" :lg="14">
-        <div class="chart-wrapper" style="height:200px;">
+        <div
+          class="chart-wrapper"
+          style="color:white; height:200px; background: #333333;"
+        >
           LAST 12 HOURS
+          <br />
+          <img v-if="lasthourimg" :src="lasthourimg" style="height:140px;" />
         </div>
       </el-col>
     </el-row>
@@ -89,6 +119,34 @@ export default {
     return {
       lineChartData: lineChartData.newVisitis,
       weatherimg: weatherBG,
+      lasthourimg: lasthourBG,
+      logo: logoSimpleThings,
+      items: [
+        {
+          icon: 'Front-door',
+          name: 'Front-door',
+          status: 'Opened',
+          time: '1:22p',
+        },
+        {
+          icon: 'Front-door',
+          name: 'Front-door',
+          status: 'Closed',
+          time: '2:34p',
+        },
+        {
+          icon: 'Bedroom Window',
+          name: 'Bedroom Window',
+          status: 'Opened & Closed',
+          time: '3:40p',
+        },
+        {
+          icon: 'Drive Way',
+          name: 'Face Detected',
+          status: 'Opened',
+          time: '5:22p',
+        },
+      ],
     };
   },
   methods: {
@@ -99,6 +157,8 @@ export default {
 };
 
 import weatherBG from '@/assets/img_src/weather_bg.png';
+import logoSimpleThings from '@/assets/img_src/simple_things_logo.png';
+import lasthourBG from '@/assets/img_src/lasthour_bg.png';
 </script>
 
 <style lang="scss" scoped>
