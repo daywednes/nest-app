ea97cfd7d8d157ed1b4b45f538270c67817ae577
hoangnhutvu <vu23071997@gmail.com>
2020-11-19 02:22:10 +0700

    Handle edited action in dashboard

--- frontend/src/views/dashboard/admin/components/Widgets/CITYZEN.vue
+++ frontend/src/views/dashboard/admin/components/Widgets/CITYZEN.vue
@@ -1,5 +1,62 @@
 <template>
-  <div class="chart-wrapper" style="height:140px;">
-    CITYZEN
+  <div>
+    <div class="chart-wrapper">
+      CITYZEN
+
+      <div style="height:280px; overflow-x: auto;">
+        <el-row
+          :gutter="32"
+          v-for="(item, index) of items"
+          :key="index"
+          style="width: 95%;
+            text-align: left; margin: 10px; "
+        >
+          <el-col :xs="20" :sm="20" :lg="20">
+           <p> {{ item.status }}</p>
+            {{ item.name }}
+           <p> {{ item.address }}</p>
+          </el-col>
+          <el-col :xs="4" :sm="4" :lg="4">
+            {{ item.time }}
+          </el-col>
+          <!-- <hr
+            style=" position: absolute;left: 0;bottom: 0;width: 100%;margin: 0;"
+          /> -->
+        </el-row>
+        more
+      </div>
+    </div>
   </div>
 </template>
+<style scoped>
+p{
+    font-size: small;
+    color: darkgray;
+    font-weight: 400;
+}
+</style>
+
+<script>
+import logoSimpleThings from '@/assets/img_src/simple_things_logo.png';
+export default {
+  data() {
+    return {
+      logo: logoSimpleThings,
+      items: [
+        {
+          address: 'Front-door Address',
+          name: 'Front-door',
+          status: 'Opened',
+          time: '1:22p',
+        },
+        {
+          address: 'Bedroom Address',
+          name: 'Bedroom',
+          status: 'Closed',
+          time: '2:34p',
+        }
+      ],
+    };
+  },
+};
+</script>
--- frontend/src/views/dashboard/admin/components/Widgets/DISARMED.vue
+++ frontend/src/views/dashboard/admin/components/Widgets/DISARMED.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="chart-wrapper" style="height:140px;">
+  <div class="chart-wrapper">
     DISARMED
+    <br />
+    <img class="img-circle" style="background: #13ce66;; " />
+    <br />
+    <p>Ready to Arm</p>
+    <br/>
+    <el-button style="font-size: large; padding: 5px 40px;" type="primary" round disabled>ARM</el-button>
   </div>
 </template>
+<style scoped>
+.img-circle {
+  margin: 20px;
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+}
+p {
+  font-size: small;
+}
+</style>
+<script>
+import logoSimpleThings from '@/assets/img_src/simple_things_logo.png';
+export default {
+  data() {
+    return {
+      logo: logoSimpleThings,
+    };
+  },
+};
+</script>
